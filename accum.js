0288054477e22fb3606994f89bcaf56879e8681b
Sarah Haman <cidhigh-wind@comcast.net>
2022-05-04 19:54:42 -0700

    Create accum.js

---
+++ accum.js
@@ -0,0 +1,11 @@
+//This time no story, no theory. The examples below show you how to write function accum:
+
+//accum("abcd") -> "A-Bb-Ccc-Dddd"
+//accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+//accum("cwAt") -> "C-Ww-Aaa-Ttt
+
+function accum(s) {
+  return [...s]
+    .map((char, index) => char.toUpperCase() + char.toLowerCase().repeat(index))
+    .join("-");
+}
